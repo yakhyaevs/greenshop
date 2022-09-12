2248322d97a1401ff51a305fb39c7c7296aee704
Samandar Yakhyaev <104832305+yakhyaevs@users.noreply.github.com>
2022-09-12 18:02:07 +0900

    Update mainn.js

--- mainn.js
+++ mainn.js
@@ -3,3 +3,8 @@ new Promise((reject, resolve) => {
   reject("Welcome to");
 }).then((value) => console.log(value));
 console.log("ended...");
+
+class Newreact {
+  constructor(car,color)
+  function mixedStructure(car,color){
+    return `${car} and ${color}}};
